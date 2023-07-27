d8ee36cfa16f279eaabb680a557fc8b61e80df0f
petsimgame <140699078+petsimgame@users.noreply.github.com>
2023-07-27 18:15:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-let petName = "寵物";
 let hunger = 100;
 let happiness = 100;
+
+function updateStatus() {
+  document.getElementById("hunger").innerText = hunger;
+  document.getElementById("happiness").innerText = happiness;
+}
+
+function feedPet() {
+  if (hunger < 100) {
+    hunger += 10;
+    updateStatus();
+    alert("寵物吃飽啦！");
+  } else {
+    alert("寵物已經很飽了，不想再吃了！");
+  }
+}
+
+function playWithPet() {
+  if (happiness < 100) {
+    happiness += 10;
+    updateStatus();
+    alert("寵物很開心！");
+  } else {
+    alert("寵物已經很開心了，不想再玩了！");
+  }
+}
+// 定義等級、遊戲次數和餵食次數的變數
 let level = 1;
 let gameCount = 0;
 let feedCount = 0;
 
-// 更新寵物狀態並顯示到網頁上
-function updateStatus() {
-  document.getElementById("petName").innerText = petName;
-  document.getElementById("hunger").innerText = hunger;
-  document.getElementById("happiness").innerText = happiness;
-  document.getElementById("level").innerText = level;
-  document.getElementById("feedCount").innerText = feedCount;
-  document.getElementById("gameCount").innerText = gameCount;
-}
-
-// 更新等級
+// 更新等級和顯示到網頁上
 function updateLevel() {
   // 每10次遊戲和餵食增加一等級
-  let totalActions = gameCount + feedCount;
-  level = Math.floor(totalActions / 10) + 1;
-  updateStatus();
+  level = 1 + Math.floor((gameCount + feedCount) / 10);
+  document.getElementById("level").innerText = level;
+}
+// 遊戲
+function playWithPet() {
+  // 遊戲邏輯
+  // ...
+
+  // 更新遊戲次數
+  gameCount++;
+
+  // 更新等級
+  updateLevel();
 }
 
 // 餵食
 function feedPet() {
   // 餵食邏輯
-  hunger += 10;
-  happiness += 5;
+  // ...
 
   // 更新餵食次數
   feedCount++;
 
-  updateStatus();
-  updateLevel();
-}
-
-// 遊戲
-function playWithPet() {
-  // 遊戲邏輯
-  happiness += 10;
-
-  // 更新遊戲次數
-  gameCount++;
-
-  updateStatus();
+  // 更新等級
   updateLevel();
 }
 
