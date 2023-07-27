55a1d7f55144471aabf2678a96135c811128a4a6
petsimgame <140699078+petsimgame@users.noreply.github.com>
2023-07-27 18:00:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,26 @@ function playWithPet() {
 
   // 更新遊戲次數
   gameCount++;
+
+  // 更新等級
+  updateLevel();
+}
+
+// 餵食
+function feedPet() {
+  // 餵食邏輯
+  // ...
+
+  // 更新餵食次數
+  feedCount++;
+
+  // 更新等級
+  updateLevel();
+}
+
+
+  // 更新遊戲次數
+  gameCount++;
   // 更新顯示
   document.getElementById("gameCount").innerText = gameCount;
   // 更新等級
