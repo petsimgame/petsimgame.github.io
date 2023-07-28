9a146846de15ffc1824f653d8ac5744462567ecd
petsimgame <140699078+petsimgame@users.noreply.github.com>
2023-07-28 12:34:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,37 @@ function playWithPet() {
     alert("寵物已經很開心了，不想再玩了！");
   }
 }
+function changePet() {
+  const petSelect = document.getElementById("petSelect");
+  const selectedPet = petSelect.value;
+
+  // 更新目前寵物的名字
+  switch (selectedPet) {
+    case "dog":
+      petName = "狗";
+      document.getElementById("petImage").src = "dog.png";
+      break;
+    case "cat":
+      petName = "貓";
+      document.getElementById("petImage").src = "cat.png";
+      break;
+    case "rabbit":
+      petName = "兔子";
+      document.getElementById("petImage").src = "rabbit.png";
+      break;
+    default:
+      petName = "寵物";
+  }
+
+  // 重新設置寵物狀態
+  hunger = 100;
+  happiness = 100;
+  level = 1;
+  gameCount = 0;
+  feedCount = 0;
+
+  // 更新網頁上的寵物狀態顯示
+  updateStatus();
+}
 
 updateStatus();
